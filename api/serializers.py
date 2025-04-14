90b6895b683e29ff15c9ac01b2dfe614f38a4dd9
Arup53 <arupkumarhowlader12@gmail.com>
2025-04-14 20:46:29 +0600

    make a serializers for models

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,31 @@
 
 
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo,User, Transaction, CardDetails
+
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
+        fields = '__all__'
+
+
+
+
+class CardDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CardDetails
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    transactions = TransactionSerializer(many=True, read_only=True)
+    carddetails = CardDetailsSerializer(read_only=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
